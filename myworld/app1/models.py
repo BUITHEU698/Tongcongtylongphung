6856f8bf0f964ebe967ac212aeb98347eddbb1c1
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-04-28 08:39:09 +0700

    1234

--- myworld/app1/models.py
+++ myworld/app1/models.py
@@ -1,3 +1,10 @@
+import email
+from pyexpat import model
 from django.db import models
+from matplotlib.style import use
 
 # Create your models here.
+class contacForm(models.Model):
+    usename = models.CharField(max_length= 25)
+    email = models.EmailField()
+    body = models.TextField()
