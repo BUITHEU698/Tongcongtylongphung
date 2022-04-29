2094e78b77e0d37f31249d87522e700b9c748c62
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-04-30 00:30:38 +0700

    123

--- myworld/app1/models.py
+++ myworld/app1/models.py
@@ -4,9 +4,13 @@ from django.db import models
 from matplotlib.style import use
 
 
-#--------------contact-------------
+#--------------contac   t-------------
 class contactModel(models.Model):
     username = models.CharField(max_length= 25)
     email = models.EmailField()
     subject = models.CharField(max_length= 100)
     message = models.TextField()
+    
+    def __str__(self):
+        return self.email
+    
--- myworld/app1/views.py
+++ myworld/app1/views.py
@@ -2,54 +2,62 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .forms import contactForm
-from .models import contactModel 
+from .models import contactModel
+from django.views import View
 
-#--------------index-------------
+# --------------index-------------
+
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
-    
 
 
-#--------------cart-------------
+# --------------cart-------------
 def cart(request):
     template = loader.get_template('cart.html')
     return HttpResponse(template.render())
 
 
-
-#--------------checkout-------------
+# --------------checkout-------------
 def checkout(request):
     template = loader.get_template('checkout.html')
     return HttpResponse(template.render())
 
 
-    
-#--------------contact-------------
-def contact(request):
-    context = {'cf':contactForm}
-    return render(request, 'contact.html', context)
-        
-def saveContact (re1uest):
-    # kiem tra xem co phai phuong thuc post k
-    if request.method == "POST":
-        cf = contacForm(request.POST)
-        # Kiem tra dieu kien nhap trong input
-        if cf.is_valid():
-            #chuyen doi thong tin cua form thanh thong tin cua model
-            save_cf = contactModel(username) 
-            #luu thong tin vaao model 
-            cf.save()
-            return HttpResponse("save success")
-    else:
-        return HttpResponse("not POST")
-    
-#--------------shop-------------    
+# --------------contact-------------
+
+class contact(View):
+    def get(self, request):
+        context = {'cf': contactForm}
+        return render(request, 'contact.html', context)
+
+    def post(self, request):
+        # kiem tra xem co phai phuong thuc post k
+        if request.method == "POST":
+            # gan thong tin cua contactForm nhap tu ban phim vao trong bien cf
+            cf = contactForm(request.POST)
+            # Kiem tra dieu kien nhap trong input: neu dieu kien dung
+            if cf.is_valid():
+                # chuyen doi thong tin cua form thanh thong tin cua model
+                save_cf = contactModel(username=cf.cleaned_data['username'], email=cf.cleaned_data['email'],
+                                    subject=cf.cleaned_data['subject'], message=cf.cleaned_data['message'])
+                # luu thong tin vaao model
+                save_cf.save()
+                return HttpResponse("save success")
+        else:
+            return HttpResponse("not POST")
+
+# --------------shop-------------
+
+
 def shop(request):
     template = loader.get_template('shop.html')
     return HttpResponse(template.render())
 
-#--------------detail-------------  
+# --------------detail-------------
+
+
 def detail(request):
     template = loader.get_template('detail.html')
     return HttpResponse(template.render())
