988b477811c144d9d0a2f71a682ee6da7c50a1b6
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-05-04 23:18:09 +0700

    123

--- myworld/app1/admin.py
+++ myworld/app1/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import contactModel
+from .models import possForm
 
 admin.site.register(contactModel)
 # Register your models here.
+admin.site.register(possForm)
--- myworld/app1/models.py
+++ myworld/app1/models.py
@@ -4,13 +4,20 @@ from django.db import models
 from matplotlib.style import use
 
 
-#--------------contac   t-------------
+# --------------contact-------------
 class contactModel(models.Model):
-    username = models.CharField(max_length= 25)
+    username = models.CharField(max_length=25)
     email = models.EmailField()
-    subject = models.CharField(max_length= 100)
+    subject = models.CharField(max_length=100)
     message = models.TextField()
-    
+
     def __str__(self):
         return self.email
-    
+
+# --------------postForm()-------------
+
+
+class possForm(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
