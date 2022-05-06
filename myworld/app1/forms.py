e8e5ea70b355a795cd68991f5d8c28c98260b766
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-05-06 19:51:58 +0700

    rè

--- myworld/app1/forms.py
+++ myworld/app1/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 # --------------contact-------------
 class contactForm(forms.Form):
-    username = forms.CharField(max_length=25)
-    email = forms.EmailField()
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=30)
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
@@ -12,7 +12,7 @@ class contactForm(forms.Form):
 # --------------memberForm-------------
 
 class memberForm(forms.Form):
-    username = forms.CharField(max_length=20)
+    username = forms.CharField(max_length=30)
     email = forms.EmailField(max_length = 30)
     # phone = forms.CharField(max_length=11)
     password = forms.CharField(max_length=20, widget = forms.PasswordInput)
@@ -20,5 +20,5 @@ class memberForm(forms.Form):
 
 
 class loginForm(forms.Form):
-    username = forms.CharField(max_length=20)
+    username = forms.CharField(max_length=30)
     password = forms.CharField(max_length=20, widget=forms.PasswordInput)
