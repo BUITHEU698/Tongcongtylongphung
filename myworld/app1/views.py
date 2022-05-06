9617cbe8d59c779b6ba58f5398524d853967d211
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-05-06 17:00:05 +0700

    1234

--- myworld/app1/views.py
+++ myworld/app1/views.py
@@ -116,7 +116,8 @@ class loginUser (View):
     def post(self, request):
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        try: 
+            user = authenticate(request, username= User.objects.get(email = username) , password=password)
         if user is not None:
             login(request, user)
             return render(request, 'order.html')
