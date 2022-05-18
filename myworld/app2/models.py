9d98b6401fde74e6f415399470e71301cc747e10
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-05-18 15:30:58 +0700

    1223123

--- myworld/app2/models.py
+++ myworld/app2/models.py
@@ -39,8 +39,7 @@ class ProductsModel(models.Model):
     productsPriceOther = models.FloatField('Giá so sánh', default= 0)
     inventory = models.FloatField('Hàng tồn kho', default= 0)
     productsPub = models.CharField('Hiển thị', max_length=250,  default="1", choices=CHOICES, blank= False, null = False)
-    productsDatePub = models.DateField('Ngày hiển thị', default=timezone.now)
-    productsTimePub = models.TimeField('Giờ hiển thị',default=timezone.now)
+    productsTimePub = models.DateTimeField('Thời gian hiển thị', blank= False, null = False )
     portfolioModel = models.ForeignKey(PortfolioModel, on_delete= models.CASCADE)
     weight = models.FloatField('Khối lượng sản phẩm', default= 0)
     def __str__(self):
