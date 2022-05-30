db1a26cda5f3aca6631f71fc9210373e0c6ec2db
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-05-31 02:04:26 +0700

    1234

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y#y%r#zucei!2+(vn6v=6($88wx81(kc@%i1&%ihmv&$w5)fhu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thuoclaolongphung.tk']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     
     # 'django_createsuperuser'
 ]
-SITE_ID=1
+SITE_ID=5
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
